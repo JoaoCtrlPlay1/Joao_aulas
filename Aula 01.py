cdfcc4dcf71b708d0ea8120e41c65ca0974325f9
karim aylen <kazinha809@gmail.com>
2025-01-22 12:17:30 -0300

    Adicionei dois arquivos

---
+++ Aula 01.py
@@ -0,0 +1,3 @@
+Aula 01 
+# Primeiro programa: Jupyter
+print ("Olá Mundo")
---
+++ Aula 02.py
@@ -0,0 +1,11 @@
+Aula 01 
+# Primeiro programa: Jupyter
+print ("Olá Mundo")
+
+Aula 02
+#Criando Strings
+nome = "João"
+sobrenome = 
+nomeCompleto+
+msg= 
+print (msg+nomCompleto)
